0da784c90622a73f9d8efdc5fbcba4121f4640a0
Lee-layoung <128294714+Lee-layoung@users.noreply.github.com>
2025-08-07 16:39:08 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,16 +5,16 @@ import streamlit as st
 import pandas as pd
 
 # 1. 제목
-st.title("Streamlit 기본 예제 페이지")
+st.title("카톨릭관동대 연수 예제 페이지")
 
 # 2. 부제목
 st.subheader("이 페이지는 다양한 Streamlit 기능을 보여줍니다.")
 
 # 3. 판다스 데이터프레임 기반 표 출력
 df = pd.DataFrame({
-    "Name": ["Alice", "Bob", "Charlie"],
-    "Age": [24, 30, 29],
-    "Country": ["Korea", "USA", "UK"]
+    "Name": ["Alice", "Bob", "Charlie", "layoung"],
+    "Age": [24, 30, 29, 48],
+    "Country": ["Korea", "USA", "UK", "Korea"]
 })
 st.write("데이터프레임 예제")
 st.dataframe(df)
